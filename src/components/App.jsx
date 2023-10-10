b5cbe90821676923e4073b085f6633dbab6b165e
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-10-10 20:42:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
 import {ContactForm} from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import {Filter} from './Filter/Filter';
