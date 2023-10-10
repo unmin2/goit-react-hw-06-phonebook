af348db41cb03ab6652d76e243daccbb7917ab76
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-10-10 20:39:16 +0300

    Update contactsSlice.js

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -2,12 +2,7 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 export const sliceContact = createSlice({
   name: 'contacts',
-  initialState: [
-    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  initialState: [],
   reducers: {
     add(state, action) {
       state.push({
